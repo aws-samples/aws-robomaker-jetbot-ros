bd83b6e4bf12345337c4d6ca4afdc5b09b4b22b4
cataherrera <cfherrera@uc.cl>
2022-01-05 16:17:17 -0300

    add robo4kids example app

--- src/jetbot_robo4kids/assets/teleop/aws-iot.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable */
-
-const awsiot = {
-  "host": "<Update IoT Endpoint Here>", // AWS MQTT Endpoint
-  "region": "<Update Region Here>", // AWS Region, ie. us-east-1
-};
-
-
-export default awsiot ;
